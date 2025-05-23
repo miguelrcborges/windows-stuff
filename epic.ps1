6dec6ff16ccae4ecb29bfac9c4e854d77412d305
Miguel Borges <up202004481@up.pt>
2025-05-23 18:19:57 +0100

    epic

--- epic.ps1
+++ epic.ps1
@@ -1,2 +1,9 @@
 # Disable memory compression
 Disable-MMAgent -MemoryCompression
+
+# Disable hibernation
+powercfg /h off
+
+# https://github.com/valleyofdoom/PC-Tuning/blob/main/README.md#1137-file-system-permalink
+fsutil behavior set disable8dot3 1
+fsutil behavior set disablelastaccess 1
